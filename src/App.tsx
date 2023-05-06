152df46e309517321d1e8fc642c105bd9af597c2
AllexKzk <san4a_kozikov@mail.ru>
2023-05-06 18:28:39 +0300

    first test commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [messageFromServer, setMessage] = useState<String>('');
+
+  useEffect(() => {
+    fetch("https://rich-teal-leopard-gown.cyclic.app/api/")
+      .then(data => data.json())
+      .then(json => setMessage(json.message))
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      It's test message!
+      <p>{messageFromServer}</p>
     </div>
   );
 }
