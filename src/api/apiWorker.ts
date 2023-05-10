54f98605915395b2dae32ed0d126fd87e4736087
AllexKzk <san4a_kozikov@mail.ru>
2023-05-10 19:06:50 +0300

    prod set

--- src/api/apiWorker.ts
+++ src/api/apiWorker.ts
@@ -3,7 +3,7 @@ import { setUser, setUserData } from "../storage/AuthUser";
 import { store } from "../storage/Store";
 import { IPostsCollection, IPost } from "../Components/Posts/IPost";
 
-export const serverUrl = 'http://localhost:5000'//'https://rich-teal-leopard-gown.cyclic.app' //'http://localhost:5000';
+export const serverUrl = 'https://rich-teal-leopard-gown.cyclic.app';
 const apiUrl = `${serverUrl}/api`;
 
 interface IBodyRequest {
